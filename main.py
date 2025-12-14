510f26b5547a4b8a6f718922964ea84d1255837c
Vansh Khaneja <vanshkhaneja08@gmail.com>
2025-12-14 14:45:49 +0530

    feat: test MongoDB and Cloudinary functionalities

--- main.py
+++ main.py
@@ -1,6 +1,39 @@
 from image_tagging import tag_image
 
+from mongodb_uploader import upload_item, get_item, delete_item, get_items, delete_items, update_item
+
+from cloudinary_uploader import upload_image, delete_image
+
+from uuid import uuid4
 # Pass image and get tagged dict
 tagged_dict = tag_image("test.jpg")
 
-print(tagged_dict)
+# url,public_id = upload_image("test.jpg")
+
+wardrobe_id = "abc123"
+
+# document = {"wardrobe_id": wardrobe_id, "item_id": str(uuid4()), "image_url": url, "tags": tagged_dict}
+
+
+# response = upload_item(document)
+
+# print(response)
+
+items = get_item("71b0a92a-54a1-40fc-bc1b-8a02c8fae800")
+print(items)
+# for item in items:
+#     print(item["item_id"])
+
+
+
+# response = delete_items(wardrobe_id)
+# print(response)
+
+
+# update_item("71b0a92a-54a1-40fc-bc1b-8a02c8fae800", {"tags": tagged_dict})
+
+response = delete_item("71b0a92a-54a1-40fc-bc1b-8a02c8fae800")
+print(response)
+
+items = get_item("71b0a92a-54a1-40fc-bc1b-8a02c8fae800")
+print(items)
